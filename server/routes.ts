ffad24216e5296f44cab63d53b856e7cbc720c0e
Richik Chanda <richikchanda1999@gmail.com>
2022-10-17 12:56:01 +0530

    chore: removed boiler code

--- server/routes.ts
+++ server/routes.ts
@@ -1,7 +1,5 @@
 import { Application } from 'express';
-import examplesRouter from './api/controllers/examples/router';
 import mappingsRouter from './api/controllers/mappings/router';
 export default function routes(app: Application): void {
-  app.use('/api/v1/examples', examplesRouter);
   app.use('/api/v1/check_mapping', mappingsRouter);
 }
