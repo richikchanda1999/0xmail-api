39d503350fb2fee1209c678ea7fcb3db76037c55
Richik Chanda <richikchanda1999@gmail.com>
2022-10-27 12:35:11 +0530

    fix: decoding log events instead of transaction

--- server/utils/decode.ts
+++ server/utils/decode.ts
@@ -1,41 +1,98 @@
 import { ethers } from 'ethers';
+import sha256 from 'crypto-js/sha256';
 import ABI from '../../abis/Communication.json';
 
+type Data = {
+  chainId?: number;
+  emailHash?: string;
+  sender?: string;
+  walletAddress?: string;
+  timestamp?: number;
+  error?: string;
+};
+
 async function decodeTransaction(chainId: number, txHash: string) {
-  console.log(chainId, txHash);
+  let provider;
+  if (chainId === 5) {
+    provider = ethers.getDefaultProvider('goerli');
+  } else if (chainId === 10) {
+    provider = ethers.getDefaultProvider('optimism');
+  } else if (chainId === 137) {
+    provider = ethers.getDefaultProvider('matic');
+  } else if (chainId === 42220) {
+    provider = new ethers.providers.JsonRpcProvider('https://forno.celo.org');
+  } else {
+    return { error: 'Invalid chain ID' };
+  }
 
-  //   const provider = ethers.getDefaultProvider('matic');
-  //   const provider = ethers.getDefaultProvider('optimism');
-
-  const provider = ethers.getDefaultProvider('goerli');
-
-  const tx = await provider.getTransaction(txHash);
-  console.log(tx);
+  const tx = await provider.getTransactionReceipt(txHash);
 
   const iface = new ethers.utils.Interface(ABI);
-  const decodedData = iface.parseTransaction({
-    data: tx.data,
-    value: tx.value,
-  });
-
-  console.log(decodedData);
-  const args = decodedData.args;
 
   try {
-    const ret = {
-      chainId: args['chainId'].toNumber(),
-      emailHash: args['emailHash'],
-      sender: args['sender'],
-    };
-    return ret;
+    let data: Data = { error: 'No event found' };
+    for (const log of tx.logs) {
+      const event = iface.parseLog(log);
+      if (event.name === 'EmailAdded') {
+        data = {
+          chainId: event.args.chainId.toNumber(),
+          emailHash: event.args.emailHash,
+          sender: event.args.sender,
+          walletAddress: event.args.walletAddress,
+          timestamp: event.args.timestamp.toNumber(),
+        };
+        break;
+      }
+    }
+
+    return data;
   } catch (e) {
-    return { error: e };
+    return { error: `Some error occurred - ${e}` };
   }
 }
 
-decodeTransaction(
+async function main(
+  chainIdFromAPI: number,
+  senderFromAPI: string,
+  email: string,
+  txHash: string
+) {
+  const ret = await decodeTransaction(chainIdFromAPI, txHash);
+  if (ret.error) {
+    console.error(`Could not decode transaction: ${ret.error}`);
+    return false;
+  }
+  console.log(ret);
+
+  const { chainId, emailHash, sender, walletAddress, timestamp } = ret;
+  if (!emailHash || !chainId || !sender || !walletAddress || !timestamp) {
+    console.error('Could not decode transaction');
+    return false;
+  }
+
+  if (chainId !== chainIdFromAPI) {
+    console.error('Chain ID mismatch');
+    return false;
+  } else if (sender !== senderFromAPI) {
+    console.error('Sender mismatch');
+    return false;
+  }
+
+  const digest = sha256(email).toString();
+
+  if (digest !== emailHash) {
+    console.error('Emails do not match');
+    return false;
+  }
+
+  return true;
+}
+
+main(
   5,
-  '0x0fcc686540e2c2ae5b7defc570bdcd195c16ed463e53f8c9162b2a3c3b644d2c'
-);
+  '0x4e35fF1872A720695a741B00f2fA4D1883440baC',
+  'richikchanda1999@gmail.com',
+  '0x343eb6087ba0582e20578b089014a52613ef7b005eb604209ec7a2f691272b35'
+).then((res) => console.log(res));
 
 export default decodeTransaction;
